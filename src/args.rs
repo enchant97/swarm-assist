e2cfe73fbe738d7cb90273daf05332233c18f833
Leo Spratt <enchant97@users.noreply.github.com>
2024-05-19 17:15:56 +0100

    add command comments

--- src/args.rs
+++ src/args.rs
@@ -2,34 +2,36 @@ use clap::{Parser, Subcommand};
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
+    /// Deploy a stack
     Deploy {
         stacks: Vec<String>,
         #[arg(short, long)]
         prune: bool,
     },
+    /// List currently deployed stacks
     LsStacks,
-    PsStack {
-        stack: String,
-    },
-    RmStack {
-        stacks: Vec<String>,
-    },
-    Recreate {
-        services: Vec<String>,
-    },
-    Rm {
-        services: Vec<String>,
-    },
-    Rollback {
-        services: Vec<String>,
-    },
+    /// Show deployed services for specific stack
+    PsStack { stack: String },
+    /// Remove a stack
+    RmStack { stacks: Vec<String> },
+    /// Force a service to rebuild and pull fresh image
+    Recreate { services: Vec<String> },
+    /// Remove a deployed service
+    Rm { services: Vec<String> },
+    /// Revert a service to previous state
+    Rollback { services: Vec<String> },
+    /// List services
     Services,
+    /// List swarm nodes
     Nodes,
+    /// Prune current node's data
     Prune {
         #[arg(short, long)]
         volumes: bool,
     },
+    /// Show Docker info for current node
     Info,
+    /// Show stats for current node
     Stats,
 }
 
